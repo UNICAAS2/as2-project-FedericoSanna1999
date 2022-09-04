21ad6685883e44849dcbe2699d9d01ba28f82290
FedericoSanna1999 <federicodedesanna@gmail.com>
2022-09-04 10:36:04 +0200

    Added the function intersection

--- utils/geometric_utils.cpp
+++ utils/geometric_utils.cpp
@@ -8,3 +8,15 @@
 double geometricUtils::slope(const cg3::Segment2d& segment) {
     return (segment.p1().y() - segment.p2().y()) / (segment.p1().x() - segment.p2().x());
 }
+
+/**
+ * @brief geometricUtils::intersection returns the point which has the x coordinate and lies on the segment.
+ * @param segment is the segment where the point to calculate lies on.
+ * @param x is the x coordinate which the point has.
+ * @return the point which has the x coordinate and lies on the segment.
+ */
+const cg3::Point2d geometricUtils::intersection(const cg3::Segment2d& segment, const double& x) {
+    const double& m = slope(segment);
+    const double q = segment.p1().y() - m * segment.p1().x();
+    return cg3::Point2d(x, m * x + q);
+}
--- utils/geometric_utils.h
+++ utils/geometric_utils.h
@@ -2,9 +2,11 @@
 #define GEOMETRIC_UTILS_H
 
 #include <cg3/geometry/segment2.h>
+#include <cg3/geometry/point2.h>
 
 namespace geometricUtils {
     double slope(const cg3::Segment2d& segment);
+    const cg3::Point2d intersection(const cg3::Segment2d& segment, const double& x);
 }
 
 #endif // GEOMETRIC_UTILS_H
